2a47d84dc9dbd7130099c08aff3852b2474bb596
Saihtam77 <Geroffroym777@gmail.com>
2024-01-05 01:49:24 +0100

    some modif on the interface: App name and sizing

--- Interface/display_main.py
+++ Interface/display_main.py
@@ -5,6 +5,7 @@ from Interface.interface_function.refresh_listbox import refresh_listbox
 
 db = connection()
 root = tk.Tk()
+root.title("StudentsGestionAPP")  # Set the title of the window
 root.geometry("300x200")  # Set initial size of the window: 300x200 pixels
 root.resizable(True, True)  # Make the window resizable in both directions
 
--- Interface/interface_function/refresh_listbox.py
+++ Interface/interface_function/refresh_listbox.py
@@ -1,10 +1,12 @@
 from Interface.common import tk
 from Fonctions.StudentsFunction import *
 
+# Show all the students in the listbox
 def refresh_listbox(root, db):
     
     listbox = tk.Listbox(root)
     listbox.pack()
+    listbox.pack(fill=tk.BOTH, expand=1)  # Make the listbox expand when the window is resized
     
     listbox.delete(0, tk.END)
     students = getAll_students(db)
